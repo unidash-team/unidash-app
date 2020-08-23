ffe348f50ea0e2ddc77383bfad066444ce63ea6d
Gino Messmer <git@ginomessmer.me>
2020-08-23 13:24:26 +0200

    Move auth service

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  register() {
+    // TODO
+  }
+
+  login() {
+    // TODO
+  }
+
+  signOut() {
+    // TODO
+  }
+
+  setToken() {
+    // TODO
+  }
+
+  getToken() {
+    // TODO
+  }
+}
