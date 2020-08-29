41b7fb9f79d566731b837c600aa3dac678316826
Gino Messmer <git@ginomessmer.me>
2020-08-29 15:33:19 +0200

    Add modiels

---
+++ src/app/features/auth/models/Login.ts
@@ -0,0 +1,5 @@
+
+export interface Login {
+  emailAddress: string;
+  password: string;
+}
---
+++ src/app/features/auth/models/Register.ts
@@ -0,0 +1,9 @@
+export interface Register {
+  firstName: string;
+  lastName: string;
+
+  emailAddress: string;
+  password: string;
+
+  dateOfBirth: Date;
+}
